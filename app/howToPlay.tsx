3f292727f998771a87636fd58f7b4ace238518de
Marek Trybuła <trybu001@gmail.com>
2025-05-06 17:21:44 +0200

    How to play screen update to scrollable view component

--- app/howToPlay.tsx
+++ app/howToPlay.tsx
@@ -1,11 +1,29 @@
 import MyText from '@/components/MyText'
-import { StyleSheet, View } from 'react-native'
+import { ScrollView, StyleSheet, View } from 'react-native'
 
 export default function HowToPlay() {
   return (
-    <View style={styles.viewWrapper}>
-      <MyText>How to play Screen</MyText>
-    </View>
+    <ScrollView style={styles.viewWrapper}>
+      <View style={styles.viewContent}>
+        <MyText bold>Ogólny zarys gry</MyText>
+        <MyText>
+          Rzuć pytanie - gra towarzyska do wspólnej zabawy w jednym
+          pomieszczeniu. Gracze rzucają wirtualną kością, odpowiadają na pytania
+          znajdujące się w bazie pytań gry lub na te zadane przez innego gracza.
+          Gra opiera się na sprawdzaniu wiedzy, zaufaniu i dobrej zabawie –
+          idealna na imprezy, spotkania i wieczory z przyjaciółmi.
+        </MyText>
+      </View>
+
+      <View style={styles.viewContent}>
+        <MyText bold>Tworzenie gry</MyText>
+        <MyText>
+          Jeden z graczy tworzy pokój, do którego dołączają później inni gracze.
+          Wybiera swoje zdjęcie, imię, kategorie pytań, ilość rund oraz
+          rozpoczyna rozgrywkę.
+        </MyText>
+      </View>
+    </ScrollView>
   )
 }
 
@@ -14,5 +32,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#2B2F41',
     padding: 24,
+  },
+
+  viewContent: {
+    marginBottom: 12,
   },
 })
--- components/MyText.tsx
+++ components/MyText.tsx
@@ -4,15 +4,23 @@ import { StyleSheet, Text, TextProps } from 'react-native'
 type MyTextProps = TextProps & {
   children: React.ReactNode
   align?: 'left' | 'center' | 'right'
+  bold?: boolean
 }
 
 const MyText: React.FC<MyTextProps> = ({
   align = 'left',
+  bold = false,
   children,
   ...props
 }) => {
   return (
-    <Text style={[styles.text, { textAlign: align }]} {...props}>
+    <Text
+      style={[
+        styles.text,
+        { textAlign: align, fontWeight: bold ? '600' : '400' },
+      ]}
+      {...props}
+    >
       {children}
     </Text>
   )
