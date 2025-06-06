9003d5d9b54af9640c3b3f0a2736468fb4bba5b7
Marek Trybuła <trybu001@gmail.com>
2025-06-06 18:44:58 +0200

    game component changes

--- app/game.tsx
+++ app/game.tsx
@@ -1,31 +1,11 @@
 import socket from '@/client/socket'
-import MyButton from '@/components/Button'
+import GamePrepareQuestions from '@/components/GamePrepareQuestions'
 import MyText from '@/components/MyText'
 import { Player, useLobbyStore } from '@/store/lobbyStore'
 import { useEffect, useState } from 'react'
-import {
-  Alert,
-  Image,
-  ScrollView,
-  StyleSheet,
-  TextInput,
-  View,
-} from 'react-native'
+import { Image, ScrollView, StyleSheet, View } from 'react-native'
 
 const Game = () => {
-  const dieImages = [
-    require('@/assets/images/regular_dies/die1.png'),
-    require('@/assets/images/regular_dies/die2.png'),
-    require('@/assets/images/regular_dies/die3.png'),
-    require('@/assets/images/regular_dies/die4.png'),
-    require('@/assets/images/regular_dies/die5.png'),
-    require('@/assets/images/regular_dies/die6.png'),
-  ]
-  const inputIds = ['input1', 'input2', 'input3', 'input4', 'input5', 'input6']
-  const [inputValues, setInputValues] = useState<string[]>(
-    Array(inputIds.length).fill(''),
-  )
-
   const [socketId, setSocketId] = useState<string | undefined>()
   const {
     accessCode,
@@ -34,20 +14,22 @@ const Game = () => {
     allCategories,
     categories,
     setPlayer,
-    setLobby,
     setPlayers,
     currentRound,
-    roundsTotal,
     playerTurnIndex,
   } = useLobbyStore((state) => state)
-  const player = players.find((p) => p.id === socketId)
-  const questionsTargetId = players.find(
-    (p) => p.id === socketId,
-  )?.questionsTargetPlayerId
-  const questionTargetPlayer = players.find((p) => p.id === questionsTargetId)
-  const playersReadyCount = players.filter((p) => p.isReady).length
 
+  const playersReadyCount = players?.filter((p) => p.isReady).length
   const currentCategory = allCategories?.find((c) => c.id === categories[0])
+  const currentQuestions = players?.flatMap((p) => p.questions || [])
+
+  type GameStatus =
+    | 'changingPlayer'
+    | 'playerTurn'
+    | 'questionPhase'
+    | 'gameOver'
+
+  let gameStatus: GameStatus = 'changingPlayer'
 
   useEffect(() => {
     if (socket.connected) {
@@ -94,147 +76,63 @@ const Game = () => {
     console.log('Aktualny gracz:', current)
   }, [players])
 
-  const handleChangeInputValue = (value: string, index: number) => {
-    setInputValues((prevValues: string[]) => {
-      const newValues = [...prevValues]
-      newValues[index] = value
-      return newValues
-    })
-  }
-
-  // W komponencie Game, w funkcji handleReadyChange
-  const handleReadyChange = (isReady: boolean) => {
-    if (inputValues.some((value) => value.trim() === '')) {
-      Alert.alert('Wszystkie pola muszą zostać wypełnione')
-      return
-    }
-
-    socket.emit(
-      'readyChange',
-      { accessCode, playerId: socketId, isReady },
-      (response: {
-        success: boolean
-        message?: string
-        arePlayersReady: boolean // Nadal potrzebne do synchronizacji startu gry
-      }) => {
-        if (!response.success) {
-          console.error('Błąd zmiany gotowości:', response.message)
-        } else {
-          const questions = inputValues.map((value, index) => ({
-            id: index,
-            question: value.trim(),
-            isCustomQuestion: true,
-            answers: null,
-            points: null,
-            isAnswered: false,
-          }))
-
-          // Wyślij pytania, gdy tylko gracz jest gotowy
-          if (isReady) {
-            socket.emit(
-              'prepareQuestions',
-              {
-                accessCode,
-                playerId: socketId,
-                questions,
-              },
-              (response: { success: boolean }) => {
-                if (response.success) {
-                  console.log('Pytania ustawione dla gracza', socketId)
-                } else {
-                  console.error('Błąd podczas ustawiania pytań')
-                }
-              },
-            )
-          }
-        }
-      },
-    )
-  }
-
-  if (hasCustomCategory && playersReadyCount < players.length)
-    return (
-      <ScrollView style={styles.viewWrapper}>
-        <View>
-          <MyText align="center" size="l">
-            Dodaj własne pytania
-          </MyText>
-          <View>
-            <MyText align="center">Zadajesz pytania graczowi</MyText>
-            <MyText align="center" color="orange">
-              {questionTargetPlayer?.playerName}
-            </MyText>
+  const renderGameScreen = (status: GameStatus) => {
+    switch (status) {
+      case 'changingPlayer':
+        return (
+          <View
+            key={players[playerTurnIndex].id}
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <Image
+              source={{ uri: players[playerTurnIndex].avatar }}
+              style={{
+                width: 156,
+                height: 156,
+                borderRadius: 100,
+                marginBottom: 12,
+              }}
+            />
+            <MyText>Odpowiada {players[playerTurnIndex].playerName}</MyText>
           </View>
-          <MyText align="center" size="s" color="gray">
-            Im więcej oczek tym pytanie powinno być trudniejsze. Pytania te są
-            anonimowe. Pytania mogą być najróżniejsze, od sprawdzenia wiedzy z
-            różnych kategorii aż po sferę prywatną. Daj upust swojej wyobraźni!
-          </MyText>
-        </View>
+        )
 
-        <View>
-          {inputIds.map((input, index) => (
-            <View key={input}>
-              <MyText align="left" size="s" color="gray">
-                Pytanie {index + 1}
-              </MyText>
-              <View style={styles.inputWrapper}>
-                <Image
-                  source={dieImages[index]}
-                  style={{ width: 24, height: 24 }}
-                />
-                <TextInput
-                  style={styles.textInput}
-                  value={inputValues[index]}
-                  onChange={(e) =>
-                    handleChangeInputValue(e.nativeEvent.text, index)
-                  }
-                ></TextInput>
-              </View>
-            </View>
-          ))}
-        </View>
+      case 'playerTurn':
+        return <MyText>Rzuć kością</MyText>
 
-        <MyButton
-          bgColor={player?.isReady ? 'green' : 'red'}
-          onPress={() => handleReadyChange(!player?.isReady)}
-        >
-          <MyText align="center">
-            {player?.isReady ? 'Gotów' : 'Niegotowy'}
-          </MyText>
-        </MyButton>
+      case 'questionPhase':
+        return <MyText>Wyświetl pytanie zgodnie z wylosowaną liczbą</MyText>
 
-        <MyText align="center" size="s" color="gray">
-          {player?.isReady
-            ? 'Jesteś gotowy, czekaj na rozpoczęcie gry'
-            : 'Aktualnie jesteś niegotowy'}
-        </MyText>
-        <MyText align="center" size="s" color="gray">
-          {playersReadyCount} / {players.length} graczy gotowych
-        </MyText>
-      </ScrollView>
-    )
+      case 'gameOver':
+        return <MyText>Gra zakończona</MyText>
 
-  if (playersReadyCount === players.length)
+      default:
+        return <MyText>Wczytywanie...</MyText>
+    }
+  }
+
+  if (socket.id && hasCustomCategory && playersReadyCount < players.length)
     return (
-      <View style={styles.viewWrapper}>
-        <View style={styles.gameHeader}>
-          <MyText>{currentCategory?.name}</MyText>
-          <MyText>Runda: {currentRound}</MyText>
-        </View>
-        <MyText>Gracz: {players[playerTurnIndex].playerName}</MyText>
-        <MyText>Gracze gotowi zaczynamy Grę</MyText>
-      </View>
+      <GamePrepareQuestions
+        players={players}
+        accessCode={accessCode}
+        playerId={socket.id}
+      />
     )
 
   return (
     <View style={styles.viewWrapper}>
       <View style={styles.gameHeader}>
-        <MyText>Kategoria</MyText>
-        <MyText>Runda 1</MyText>
+        <MyText>{currentCategory?.name}</MyText>
+        <MyText>Runda: {currentRound}</MyText>
       </View>
-      <MyText>Gracz: </MyText>
-      <MyText>Game Screen</MyText>
+
+      <ScrollView style={styles.gameBody}>
+        {renderGameScreen(gameStatus)}
+      </ScrollView>
     </View>
   )
 }
@@ -259,22 +157,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 
-  inputWrapper: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 14,
-    padding: 12,
-    borderWidth: 2,
-    borderColor: '#FF9D00',
-    borderRadius: 24,
-  },
-
-  textInput: {
+  gameBody: {
     flex: 1,
-    marginLeft: 12,
     paddingVertical: 12,
-    color: '#FFFFFF',
-    fontSize: 14,
-    fontFamily: 'MuseoModerno',
+    backgroundColor: 'orange',
   },
 })
---
+++ components/GamePrepareQuestions.tsx
@@ -0,0 +1,188 @@
+import socket from '@/client/socket'
+import MyButton from '@/components/Button'
+import MyText from '@/components/MyText'
+import { Player } from '@/store/lobbyStore'
+import { useState } from 'react'
+import {
+  Alert,
+  Image,
+  ScrollView,
+  StyleSheet,
+  TextInput,
+  View,
+} from 'react-native'
+
+type GamePrepareQuestionsProps = {
+  players: Player[]
+  accessCode: string
+  playerId: string
+}
+
+const GamePrepareQuestions: React.FC<GamePrepareQuestionsProps> = ({
+  players,
+  accessCode,
+  playerId,
+}) => {
+  const dieImages = [
+    require('@/assets/images/regular_dies/die1.png'),
+    require('@/assets/images/regular_dies/die2.png'),
+    require('@/assets/images/regular_dies/die3.png'),
+    require('@/assets/images/regular_dies/die4.png'),
+    require('@/assets/images/regular_dies/die5.png'),
+    require('@/assets/images/regular_dies/die6.png'),
+  ]
+  const inputIds = ['input1', 'input2', 'input3', 'input4', 'input5', 'input6']
+  const [inputValues, setInputValues] = useState<string[]>(
+    Array(inputIds.length).fill(''),
+  )
+
+  const player = players?.find((p) => p.id === playerId)
+  const questionsTargetId = players?.find(
+    (p) => p.id === playerId,
+  )?.questionsTargetPlayerId
+  const questionTargetPlayer = players?.find((p) => p.id === questionsTargetId)
+  const playersReadyCount = players?.filter((p) => p.isReady).length
+
+  const handleChangeInputValue = (value: string, index: number) => {
+    setInputValues((prevValues: string[]) => {
+      const newValues = [...prevValues]
+      newValues[index] = value
+      return newValues
+    })
+  }
+
+  const handleReadyChange = (isReady: boolean) => {
+    if (inputValues.some((value) => value.trim() === '')) {
+      Alert.alert('Wszystkie pola muszą zostać wypełnione')
+      return
+    }
+
+    if (isReady) {
+      const questions = inputValues.map((value, index) => ({
+        id: Math.random().toString(36).substring(2, 15),
+        question: value.trim(),
+        isCustomQuestion: true,
+        answers: null,
+        points: null,
+        isAnswered: false,
+        difficulty: index + 1,
+      }))
+
+      socket.emit(
+        'prepareQuestions',
+        { accessCode, playerId, questions },
+        (response: { success: boolean }) => {
+          if (response.success) {
+            // Dopiero teraz ustaw gotowość
+            socket.emit(
+              'readyChange',
+              { accessCode, playerId, isReady },
+              (readyResponse: { success: boolean; message: string }) => {
+                if (!readyResponse.success) {
+                  console.error('Błąd zmiany gotowości:', readyResponse.message)
+                }
+              },
+            )
+          } else {
+            console.error('Błąd podczas ustawiania pytań')
+          }
+        },
+      )
+    } else {
+      // Jeśli cofamy gotowość
+      socket.emit('readyChange', { accessCode, playerId, isReady }, () => {})
+    }
+  }
+
+  return (
+    <ScrollView style={styles.viewWrapper}>
+      <View>
+        <MyText align="center" size="l">
+          Dodaj własne pytania
+        </MyText>
+        <View>
+          <MyText align="center">Zadajesz pytania graczowi</MyText>
+          <MyText align="center" color="orange">
+            {questionTargetPlayer?.playerName}
+          </MyText>
+        </View>
+        <MyText align="center" size="s" color="gray">
+          Im więcej oczek tym pytanie powinno być trudniejsze. Pytania te są
+          anonimowe. Pytania mogą być najróżniejsze, od sprawdzenia wiedzy z
+          różnych kategorii aż po sferę prywatną. Daj upust swojej wyobraźni!
+        </MyText>
+      </View>
+
+      <View>
+        {inputIds.map((input, index) => (
+          <View key={input}>
+            <MyText align="left" size="s" color="gray">
+              Pytanie {index + 1}
+            </MyText>
+            <View style={styles.inputWrapper}>
+              <Image
+                source={dieImages[index]}
+                style={{ width: 24, height: 24 }}
+              />
+              <TextInput
+                style={styles.textInput}
+                value={inputValues[index]}
+                onChange={(e) =>
+                  handleChangeInputValue(e.nativeEvent.text, index)
+                }
+              ></TextInput>
+            </View>
+          </View>
+        ))}
+      </View>
+
+      <MyButton
+        bgColor={player?.isReady ? 'green' : 'red'}
+        onPress={() => handleReadyChange(!player?.isReady)}
+      >
+        <MyText align="center">
+          {player?.isReady ? 'Gotów' : 'Niegotowy'}
+        </MyText>
+      </MyButton>
+
+      <MyText align="center" size="s" color="gray">
+        {player?.isReady
+          ? 'Jesteś gotowy, czekaj na rozpoczęcie gry'
+          : 'Aktualnie jesteś niegotowy'}
+      </MyText>
+      <MyText align="center" size="s" color="gray">
+        {playersReadyCount} / {players.length} graczy gotowych
+      </MyText>
+    </ScrollView>
+  )
+}
+
+export default GamePrepareQuestions
+
+const styles = StyleSheet.create({
+  viewWrapper: {
+    flex: 1,
+    backgroundColor: '#2B2F41',
+    paddingHorizontal: 24,
+    paddingVertical: 48,
+  },
+
+  inputWrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 14,
+    padding: 12,
+    borderWidth: 2,
+    borderColor: '#FF9D00',
+    borderRadius: 24,
+  },
+
+  textInput: {
+    flex: 1,
+    marginLeft: 12,
+    paddingVertical: 12,
+    color: '#FFFFFF',
+    fontSize: 14,
+    fontFamily: 'MuseoModerno',
+  },
+})
